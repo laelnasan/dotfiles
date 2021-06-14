499e7fccf8033887264da57064f63df2c2640c59
Lael Nascimento Santos <lael.nasan@gmail.com>
2021-06-13 20:39:22 -0400

    update the installation script

--- .config/install_packages.sh
+++ .config/install_packages.sh
@@ -5,8 +5,10 @@ sudo apt upgrade -y
 
 ###### setup env for running Goneovim and xcape
 sudo apt install -y libnss3 xsel xcape
+
 # install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 
 ###### configure git
 read -p "git config user name: " input
